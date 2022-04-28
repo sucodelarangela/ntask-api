ec04eeac088c1c6fbc463cf7254bcc6c9a655935
Angela Caldas <aes.caldas@gmail.com>
2022-04-28 13:17:42 -0300

    Fix body parser require

--- middlewares.js
+++ middlewares.js
@@ -1,3 +1,5 @@
+const bodyParser = require('body-parser')
+
 module.exports = app => {
   app.set('port', 3000) // configures the running port
   app.set('json spaces', 4) // formats json view on browser
