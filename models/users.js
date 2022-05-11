81096553acb6d106a1a1d124c9efbbcc6031c743
Angela Caldas <aes.caldas@gmail.com>
2022-05-11 13:16:20 -0300

    Using bcrypt to encrypt the user password

--- models/users.js
+++ models/users.js
@@ -1,4 +1,5 @@
-const {DataTypes} = require('sequelize')
+const bcrypt = require('bcrypt'); // importing bcrypt
+const {DataTypes} = require('sequelize');
 
 module.exports = app => {
   const Users = app.db.define('Users', {
@@ -19,6 +20,12 @@ module.exports = app => {
       allowNull: false,
       validate: {
         notEmpty: true
+      },
+      // using bcrypt to encrypt the user password before saving it in db
+      set(value) {
+        const salt = bcrypt.genSaltSync();
+        const password = bcrypt.hashSync(value, salt);
+        this.setDataValue('password', password);
       }
     },
     email: {
@@ -29,7 +36,11 @@ module.exports = app => {
         notEmpty: true
       }
     }
-  })
+  });
 
-  return Users
-}
+  return Users;
+};
+
+/*
+Everytime a value is defined for password field in db (for example: Users.create({password: '123'})), this value will be redefined with an encrypted password created by the combination of bcrypt.genSaltSync() and bcrypt.hashSync() functions. This new encrypted value will be returned with this.setDataValue()
+*/
